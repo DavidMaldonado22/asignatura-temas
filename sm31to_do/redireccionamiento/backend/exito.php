63f5459ed0489b667891c4518960ffd9677c13e7
David Maldonado <arathmaldonadomarquez@gmail.com>
2024-06-24 11:06:34 -0500

    Primer Commit

---
+++ sm31to_do/redireccionamiento/backend/exito.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exito</title>
+</head>
+
+<body>
+    <h1>Libro registrado correctamente</h1>
+    <a href="../index.php">Regresar al menu</a>
+</body>
+
+</html>
---
+++ sm31to_do/redireccionamiento/backend/registrar_libros.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+
+Cadena de conexión
+lectura de valores recibidos por POST o GET
+Petición SQL para insertar
+
+
+*/
+
+
+$consulta = false;
+
+
+
+if ($consulta) {
+    header("location: exito.php");
+} else {
+    header("location: error.php");
+}
